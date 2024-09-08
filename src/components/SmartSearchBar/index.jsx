54e6293477ba153d72e39f15a3f7edb8bda2252a
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-09-08 11:28:09 -0700

    added chips and icons

--- src/components/SmartSearchBar/index.jsx
+++ src/components/SmartSearchBar/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import CounterComponent from "../Counter";
 import { OPD_SearchBar } from "./styled";
-import { TextField , OutlinedInput, InputAdornment, IconButton} from "@material-ui/core";
+import { TextField , OutlinedInput, InputAdornment, Chip} from "@material-ui/core";
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 export const SmartSearchBar = () => {
   /*IMPORTANT run this everytime you add a new dep to not see the error : npm add -D @storybook/react-vite@latest  */
@@ -14,9 +16,19 @@ export const SmartSearchBar = () => {
   /* last stable version of Muiv4 : https://github.com/mui/material-ui/releases/tag/v4.11.3 */
   const options = [{label : 'one', id: 1}, {label : 'two', id:2}, {label : 'three', id: 3}]
 
+  const smallStyle = {
+    display: 'block',
+    padding: '0',
+    fontSize: '0.7rem',
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+    fontWeight: '400',
+    lineHeight: '1',
+    letterSpacing: '0.00938em',
+  }
+
   return (
-    <div>
-      <Autocomplete
+    <div style={{maxWidth : '36rem'}}>
+      <OPD_SearchBar
          options={options}
          getOptionLabel={(option) => option.label.toString()}
         //  renderInput={(params) =>  <TextField {...params}   />}
@@ -26,15 +38,14 @@ export const SmartSearchBar = () => {
               {...params}
               variant="standard"
               label="What are you looking for?"
-              placeholder="Favorites"
+              placeholder="find a movie"
               fullWidth
               InputProps={{
                 ...params.InputProps,
                 startAdornment: (
                   <>
                     <InputAdornment position="start">
-                      {/* <SearchIcon /> */}
-                      mag
+                      <SearchIcon />
                     </InputAdornment>
                     {params.InputProps.startAdornment}
                   </>
@@ -42,12 +53,12 @@ export const SmartSearchBar = () => {
 
                 endAdornment: (
                   <div style={{display: 'flex', flexDirection: 'column'}}>
-                    <small style={{display: 'block'}}>Popular Search Terms</small>
-                    <div>
-                      <button onClick={()=>alert('see Action list')}>Action</button>
-                      <button onClick={()=>alert('see Comedy list')}>Comedy</button>
-                      <button onClick={()=>alert('see Adventure List')}>Adventure</button>
-                      <button onClick={()=>alert('see Clear Search')}>X</button>
+                    <small style={smallStyle}>Popular Search Terms</small>
+                    <div style={{margin: '0.5rem 0'}}>
+                      <Chip label={'Action'} onClick={()=>alert('see Action list')} size={'small'}/>
+                      <Chip label={'Comedy'} onClick={()=>alert('see Comedy list')} size={'small'}/>
+                      <Chip label={'Adventure'} onClick={()=>alert('see Adventure List')} size={'small'}/>
+                      <Chip label={'X'} onClick={()=>alert('see Clear Search')} size={'small'}/>
                     </div>
                   </div>
                 )
@@ -56,7 +67,7 @@ export const SmartSearchBar = () => {
           );
         }}
          />       
-      <CounterComponent />
+      {/* <CounterComponent /> */}
     </div>
   );
 };
