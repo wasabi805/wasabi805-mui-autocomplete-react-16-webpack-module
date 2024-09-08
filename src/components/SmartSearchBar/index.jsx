7ae5e6e0bab101fa9b3547f47765337e456ea29f
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-09-08 09:35:58 -0700

    success install and added Muiv4 AutoComplete input.

--- src/components/SmartSearchBar/index.jsx
+++ src/components/SmartSearchBar/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import CounterComponent from "../Counter";
+import { OPD_SearchBar } from "./styled";
+import { TextField } from "@material-ui/core";
 
 export const SmartSearchBar = () => {
+  /*IMPORTANT run this everytime you add a new dep to not see the error : npm add -D @storybook/react-vite@latest  */
+
+  /* https://stackoverflow.com/a/66813924/7857134 | resolves candidate.toLowerCase is not a function. (In 'candidate.toLowerCase()', 'candidate.toLowerCase' is undefined) Material UI Error */
+  
+
+  /* last stable version of Muiv4 : https://github.com/mui/material-ui/releases/tag/v4.11.3 */
+  const options = [{label : 'test', id: 1}]
+
   return (
     <div>
+      <OPD_SearchBar
+         options={options}
+         getOptionLabel={(option) => option.label.toString()}
+         renderInput={(params) => <TextField {...params} />}
+         />
       <CounterComponent />
     </div>
   );
---
+++ src/components/SmartSearchBar/styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+
+/*NOTES: you need to also instal' @material-ui/lab to keep the autocomplete stable
+https://stackoverflow.com/a/61162294/7857134 
+*/
+
+
+
+
+export const OPD_SearchBar = styled(Autocomplete)(({props})=>{
+    return{}
+})
